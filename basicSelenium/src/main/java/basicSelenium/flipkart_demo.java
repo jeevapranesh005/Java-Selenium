43f74754c7bc1b6ca5faa8861ae1e3a3518083be
Jeeva Pranesh P <166803755+jeevapranesh005@users.noreply.github.com>
2026-04-01 12:49:44 +0530

    edge

--- basicSelenium/src/main/java/basicSelenium/flipkart_demo.java
+++ basicSelenium/src/main/java/basicSelenium/flipkart_demo.java
@@ -3,11 +3,12 @@ package basicSelenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class flipkart_demo {
 	public static void main(String[] args) {
 
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.flipkart.com/");
 
 		String title = driver.getTitle();
