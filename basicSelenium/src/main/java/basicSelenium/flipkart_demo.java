4bfc66bd378dccc3c02ddbad089fe76d55651c12
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-04-01 12:19:42 +0530

    2nd commit

--- basicSelenium/src/main/java/basicSelenium/flipkart_demo.java
+++ basicSelenium/src/main/java/basicSelenium/flipkart_demo.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class flipkart {
+public class flipkart_demo {
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
