33f1809ad13bb39d99b7aa74885db4920d7245f0
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-04-09 09:38:05 +0530

    new

--- basicSelenium/src/main/java/table/App1.java
+++ basicSelenium/src/main/java/table/App1.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class App1 {
-
+//
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
