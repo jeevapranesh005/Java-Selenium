9b7aafd2b7c027b8a247a6ddc47e6aaa1b24bdd4
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-04-16 09:12:17 +0530

    16-4-26

--- basicSelenium/src/main/java/ScreenShort/Screenshot.java
+++ basicSelenium/src/main/java/ScreenShort/Screenshot.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//
 public class Screenshot {
 
 	public static void main(String[] args) {
